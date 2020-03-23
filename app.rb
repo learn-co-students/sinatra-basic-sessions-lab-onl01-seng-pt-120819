bcf311ba991cd26c9df20674e837304466f20cf0
8ceg6 <8ceg6@comcast.net>
2020-03-23 14:17:12 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        erb :index
+    end
+    
+    configure do 
+        enable :sessions 
+        set :sessions_secret, "look"
+    end
+
+    post '/checkout' do 
+        @sessions = session 
+        item = params["item"]
+        @sessions[:item] = item 
+        # binding.pry
+    end 
 end
