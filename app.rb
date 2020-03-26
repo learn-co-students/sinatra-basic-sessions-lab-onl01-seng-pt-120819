483798eb63370d3ac0058c412b0250391b8748ac
pledgedavis <pledgedavis14@gmail.com>
2020-03-25 22:26:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        erb :'index'
+    end   
+   
+    configure do
+        enable :sessions 
+        set :session_secret, "pledge10"
+    end 
+    
+    post '/checkout' do
+        @session = session 
+        item = params[:item]
+        @session[:item] = item 
+    end
+
+
 end
